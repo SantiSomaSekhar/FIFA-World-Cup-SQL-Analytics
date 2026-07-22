61af2e7413a0e1fbabb84161ac93e9c63febc13b
Soma Sekhar <somasekharsanti@gmail.com>
2026-07-22 14:56:47 +0530

    Create project insights file
    Summarizes key insights from FIFA World Cup SQL analysis for decision-making.

---
+++ SQL/07_project_insights.sql
@@ -0,0 +1,16 @@
+/*
+=========================================================
+FIFA WORLD CUP SQL ANALYTICS PROJECT
+
+File: 07_project_insights.sql
+
+Description:
+Summarizes the key insights obtained from
+the FIFA World Cup SQL analysis.
+
+These insights can be used by coaches,
+analysts, recruiters, and management for
+better decision-making.
+
+=========================================================
+*/
